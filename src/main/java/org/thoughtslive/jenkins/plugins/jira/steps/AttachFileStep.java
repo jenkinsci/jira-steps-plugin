4e782417f131281f4d3c7b6ab284908640d24eb3
obreitenbach <o.breitenbach@sprengnetter.de>
2017-06-21 07:53:13 +0200

    Added steps to download attachments and attach files to an issue

---
+++ src/main/java/org/thoughtslive/jenkins/plugins/jira/steps/AttachFileStep.java
@@ -0,0 +1,108 @@
+package org.thoughtslive.jenkins.plugins.jira.steps;
+
+import hudson.Extension;
+import hudson.Util;
+import lombok.Getter;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.StepExecution;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.thoughtslive.jenkins.plugins.jira.api.ResponseData;
+import org.thoughtslive.jenkins.plugins.jira.util.JiraStepDescriptorImpl;
+import org.thoughtslive.jenkins.plugins.jira.util.JiraStepExecution;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.thoughtslive.jenkins.plugins.jira.util.Common.buildErrorResponse;
+
+/**
+ * @author Oliver Breitenbach
+ * @version 1.0.0
+ */
+public class AttachFileStep extends BasicJiraStep {
+
+    @Getter
+    private final String idOrKey;
+
+    @Getter
+    private final String attachmentPath;
+
+    @DataBoundConstructor
+    public AttachFileStep(final String idOrKey, final String attachmentPath) {
+        this.idOrKey = idOrKey;
+        this.attachmentPath = attachmentPath;
+    }
+
+    @Override
+    public StepExecution start(StepContext context) throws Exception {
+        return new Execution(this, context);
+    }
+
+    @Extension
+    public static class DescriptorImpl extends JiraStepDescriptorImpl {
+
+        @Override
+        public String getFunctionName() {
+            return "jiraAttachFile";
+        }
+
+        @Override
+        public String getDisplayName() {
+            return getPrefix() + "Attach a file to an issue";
+        }
+    }
+
+    public static class Execution extends JiraStepExecution<ResponseData<Object>> {
+
+        private static final long serialVersionUID = -4127722455057889625L;
+
+        private final AttachFileStep step;
+
+        protected Execution(final AttachFileStep step, final StepContext context)
+                throws IOException, InterruptedException {
+            super(context);
+            this.step = step;
+        }
+
+        @Override
+        protected <T> ResponseData<T> verifyInput() throws Exception {
+            String errorMessage = null;
+            ResponseData<T> response = verifyCommon(step);
+
+            if (response == null) {
+                final String idOrKey = Util.fixEmpty(step.getIdOrKey());
+                final File attachment = new File(step.getAttachmentPath());
+
+                if (idOrKey == null) {
+                    errorMessage = "idOrKey is empty or null";
+                }
+
+                if (!attachment.exists()) {
+                    errorMessage = "attachment does not exist";
+                    return buildErrorResponse(new RuntimeException(errorMessage));
+                }
+
+                if (!attachment.canRead()) {
+                    errorMessage = "no reading-permissions on file " + attachment.getAbsolutePath();
+                    return buildErrorResponse(new RuntimeException(errorMessage));
+                }
+
+                if (errorMessage != null) {
+                    response = buildErrorResponse(new RuntimeException(errorMessage));
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected ResponseData<Object> run() throws Exception {
+            ResponseData<Object> responseData = verifyInput();
+
+            if (responseData == null) {
+                logger.println("JIRA: Site - " + siteName + " - Attaching file: " + new File(step.getAttachmentPath()).getAbsolutePath());
+                responseData = jiraService.attachFile(step.getIdOrKey(), new File(step.getAttachmentPath()));
+            }
+            return logResponse(responseData);
+        }
+    }
+}
---
+++ src/main/java/org/thoughtslive/jenkins/plugins/jira/steps/DownloadAttachmentStep.java
@@ -0,0 +1,110 @@
+package org.thoughtslive.jenkins.plugins.jira.steps;
+
+import hudson.Extension;
+import hudson.Util;
+import lombok.Getter;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.jenkinsci.plugins.workflow.steps.StepExecution;
+import org.kohsuke.stapler.DataBoundConstructor;
+import org.thoughtslive.jenkins.plugins.jira.api.ResponseData;
+import org.thoughtslive.jenkins.plugins.jira.util.JiraStepDescriptorImpl;
+import org.thoughtslive.jenkins.plugins.jira.util.JiraStepExecution;
+
+import java.io.File;
+import java.io.IOException;
+
+import static org.thoughtslive.jenkins.plugins.jira.util.Common.buildErrorResponse;
+
+/**
+ * @author Oliver Breitenbach
+ * @version 1.0.0
+ */
+public class DownloadAttachmentStep extends BasicJiraStep {
+
+    @Getter
+    private final String attachmentName;
+
+    @Getter
+    private final String idOrKey;
+
+    @Getter
+    private final String targetLocation;
+
+    @DataBoundConstructor
+    public DownloadAttachmentStep(final String attachmentName, final String idOrKey, final String targetLocation) {
+        this.attachmentName = attachmentName;
+        this.idOrKey = idOrKey;
+        this.targetLocation = targetLocation;
+    }
+
+    @Override
+    public StepExecution start(final StepContext context) throws Exception {
+        return new Execution(this, context);
+    }
+
+    @Extension
+    public static class DescriptorImpl extends JiraStepDescriptorImpl {
+
+        @Override
+        public String getFunctionName() {
+            return "jiraDownloadAttachment";
+        }
+
+        @Override
+        public String getDisplayName() {
+            return getPrefix() + "Download a file which is attached to an issue ";
+        }
+    }
+
+    public static class Execution extends JiraStepExecution<ResponseData<Object>> {
+
+        private final DownloadAttachmentStep step;
+
+        protected Execution(final DownloadAttachmentStep step, final StepContext context)
+                throws IOException, InterruptedException {
+            super(context);
+            this.step = step;
+        }
+
+        @Override
+        protected <T> ResponseData<T> verifyInput() throws Exception {
+            String errorMessage = null;
+            ResponseData<T> response = verifyCommon(step);
+
+            final String idOrKey = Util.fixEmpty(step.getIdOrKey());
+            final String attachmentName = step.getAttachmentName();
+            String targetLocation = step.getTargetLocation();
+
+            if (response == null) {
+                if (idOrKey == null || idOrKey.isEmpty()) {
+                    errorMessage = "ID or key is null or empty";
+                }
+
+                if (attachmentName == null || attachmentName.isEmpty()) {
+                    errorMessage = "Attachment-Name is null or emtpy";
+                }
+
+                if (targetLocation == null || targetLocation.isEmpty()) {
+                    targetLocation = System.getProperty("user.home");
+                }
+
+                if (errorMessage != null) {
+                    response = buildErrorResponse(new RuntimeException(errorMessage));
+                }
+            }
+            return response;
+        }
+
+        @Override
+        protected ResponseData<Object> run() throws Exception {
+            ResponseData<Object> responseData = verifyInput();
+
+            if (responseData == null) {
+                logger.println("JIRA: Site - " + siteName + " - Downloading file: " + new File(step.getAttachmentName())
+                        + "from " + step.getIdOrKey() + "to " + step.getTargetLocation());
+                responseData = jiraService.downloadFile(step.getIdOrKey(), step.getAttachmentName(), step.getTargetLocation());
+            }
+            return logResponse(responseData);
+        }
+    }
+}
