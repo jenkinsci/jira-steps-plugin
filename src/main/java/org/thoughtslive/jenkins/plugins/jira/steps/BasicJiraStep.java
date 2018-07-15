f7fefd2972e4ec3a56669f2481223d490d87088c
Rayapati,Naresh <naresh.rayapati@yahoo.com>
2018-07-15 17:23:55 -0500

    JENKINS-50764 Whitelist ResponseData getData signature in In-process script approval

--- src/main/java/org/thoughtslive/jenkins/plugins/jira/steps/BasicJiraStep.java
+++ src/main/java/org/thoughtslive/jenkins/plugins/jira/steps/BasicJiraStep.java
@@ -1,7 +1,11 @@
 package org.thoughtslive.jenkins.plugins.jira.steps;
 
+import hudson.Extension;
+import java.io.IOException;
 import java.io.Serializable;
 import lombok.Getter;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.ProxyWhitelist;
+import org.jenkinsci.plugins.scriptsecurity.sandbox.whitelists.StaticWhitelist;
 import org.jenkinsci.plugins.workflow.steps.Step;
 import org.kohsuke.stapler.DataBoundSetter;
 
@@ -25,4 +29,14 @@ public abstract class BasicJiraStep extends Step implements Serializable {
   @Getter
   @DataBoundSetter
   private boolean auditLog = true;
+
+  @Extension
+  public static class JiraWhitelist extends ProxyWhitelist {
+
+    public JiraWhitelist() throws IOException {
+      super(new StaticWhitelist(
+          "method org.thoughtslive.jenkins.plugins.jira.api.ResponseData getData"
+      ));
+    }
+  }
 }
