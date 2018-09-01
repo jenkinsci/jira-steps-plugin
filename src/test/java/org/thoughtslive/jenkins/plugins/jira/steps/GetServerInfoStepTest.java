01d09094781de4a7fe183b4ab0a8011584f23afc
Rayapati,Naresh <naresh.rayapati@cerner.com>
2018-09-01 07:18:53 -0500

    JENKINS-53044 Add step to retrieve jira server info

---
+++ src/test/java/org/thoughtslive/jenkins/plugins/jira/steps/GetServerInfoStepTest.java
@@ -0,0 +1,91 @@
+package org.thoughtslive.jenkins.plugins.jira.steps;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import hudson.EnvVars;
+import hudson.model.Run;
+import hudson.model.TaskListener;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.util.Map;
+import org.jenkinsci.plugins.workflow.steps.StepContext;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.thoughtslive.jenkins.plugins.jira.Site;
+import org.thoughtslive.jenkins.plugins.jira.api.ResponseData;
+import org.thoughtslive.jenkins.plugins.jira.api.ResponseData.ResponseDataBuilder;
+import org.thoughtslive.jenkins.plugins.jira.service.JiraService;
+
+/**
+ * Unit test cases for GetServerInfoStep class.
+ *
+ * @author Stuart Rowe
+ */
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({GetServerInfoStep.class, Site.class})
+public class GetServerInfoStepTest {
+
+  @Mock
+  TaskListener taskListenerMock;
+  @Mock
+  Run<?, ?> runMock;
+  @Mock
+  EnvVars envVarsMock;
+  @Mock
+  PrintStream printStreamMock;
+  @Mock
+  JiraService jiraServiceMock;
+  @Mock
+  Site siteMock;
+  @Mock
+  StepContext contextMock;
+
+  private GetServerInfoStep.Execution stepExecution;
+
+  @Before
+  public void setup() throws IOException, InterruptedException {
+
+    // Prepare site.
+    when(envVarsMock.get("JIRA_SITE")).thenReturn("LOCAL");
+    when(envVarsMock.get("BUILD_URL")).thenReturn("http://localhost:8080/jira-testing/job/01");
+
+    PowerMockito.mockStatic(Site.class);
+    Mockito.when(Site.get(any())).thenReturn(siteMock);
+    when(siteMock.getService()).thenReturn(jiraServiceMock);
+
+    when(runMock.getCauses()).thenReturn(null);
+    when(taskListenerMock.getLogger()).thenReturn(printStreamMock);
+    doNothing().when(printStreamMock).println();
+
+    final ResponseDataBuilder<Map<String,Object>> builder = ResponseData.builder();
+    when(jiraServiceMock.getServerInfo())
+        .thenReturn(builder.successful(true).code(200).message("Success").build());
+    when(contextMock.get(Run.class)).thenReturn(runMock);
+    when(contextMock.get(TaskListener.class)).thenReturn(taskListenerMock);
+    when(contextMock.get(EnvVars.class)).thenReturn(envVarsMock);
+  }
+
+  @Test
+  public void testSuccessfulGetServerInfo() throws Exception {
+    final GetServerInfoStep step = new GetServerInfoStep();
+    stepExecution = new GetServerInfoStep.Execution(step, contextMock);
+
+    // Execute Test.
+    stepExecution.run();
+
+    // Assert Test
+    verify(jiraServiceMock, times(1)).getServerInfo();
+    assertThat(step.isFailOnError()).isEqualTo(true);
+  }
+}
